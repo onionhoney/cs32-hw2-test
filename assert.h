ed71fd7a91b8c62b57f827ff990355cd50a6e227
onionhoney <zhouhengsun@gmail.com>
2016-02-01 19:42:10 -0800

    Written macro 'check' to print test case.

--- assert.h
+++ assert.h
@@ -1,6 +1,15 @@
 #include <cstdio>
-inline
 extern void handle_assert (const char *msg, const char *file, int line){
     printf("Assertion failed: %s, file %s, line %d.\n", msg, file, line);
 }
-#define assert(EX) (void)((EX) || (handle_assert (#EX, __FILE__, __LINE__),0))
+bool handle(const char *msg, bool result){
+    if (result){
+        return true;              
+    }
+    else{
+        printf("Failed at: %s\n", msg);
+        return false;
+    }
+}
+#define check(EX) (handle(#EX, EX))
+//#define assert(EX) (void)((EX) || (handle_assert (#EX, __FILE__, __LINE__),0) )
--- test_eval.cpp
+++ test_eval.cpp
@@ -4,7 +4,23 @@
 #include "Map.h"
 #include <iostream>
 #include <string>
+#include "assert.h"
+#define check(EX) (handle(#EX, EX))
+
 using namespace std;
+string message;
+
+bool handle(const char *msg, bool result){
+    if (result){
+        message = "Success";
+        return true;              
+    }
+    else{
+        message = msg;
+        return false;
+    }
+}
+
 
 int evaluate(std::string infix, const Map& values,
              std::string& postfix, int& result);
@@ -27,78 +43,78 @@ bool test(int testIndex) {
 
     switch (testIndex) {
     case 0:
-        return (evaluate("a+ e", m, pf, answer) == 0  &&
+        return check(evaluate("a+ e", m, pf, answer) == 0  &&
                pf == "ae+"  &&  answer == -6);
 
     case 1:
-        return (evaluate("     (a+e  )", m, pf, answer) == 0 &&
+        return check(evaluate("     (a+e  )", m, pf, answer) == 0 &&
                pf == "ae+"  &&  answer == -6);
 
     case 2:
-        return (evaluate("     (a+e  )", m, pf, answer) == 0 &&
+        return check(evaluate("     (a+e  )", m, pf, answer) == 0 &&
                pf == "ae+"  &&  answer == -6);
 
     case 3:
-        return (evaluate("", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("", m, pf, answer) == 1  &&  answer == 999);
 
     case 4:
-        return (evaluate("a+", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("a+", m, pf, answer) == 1  &&  answer == 999);
 
     case 5:
-        return (evaluate("a)", m, pf, answer) == 1 && answer == 999);
+        return check(evaluate("a)", m, pf, answer) == 1 && answer == 999);
 
     case 6:
-        return (evaluate("a i", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("a i", m, pf, answer) == 1  &&  answer == 999);
 
     case 7:
-        return (evaluate("ai", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("ai", m, pf, answer) == 1  &&  answer == 999);
 
     case 8:
-        return (evaluate("ai+", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("ai+", m, pf, answer) == 1  &&  answer == 999);
 
     case 9:
-        return (evaluate("()", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("()", m, pf, answer) == 1  &&  answer == 999);
 
     case 10:
-        return (evaluate("y(o+u)", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("y(o+u)", m, pf, answer) == 1  &&  answer == 999);
 
     case 11:
-        return (evaluate("a+E", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("a+E", m, pf, answer) == 1  &&  answer == 999);
 
     case 12:
-        return (evaluate("(a+(i-o)", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("(a+(i-o)", m, pf, answer) == 1  &&  answer == 999);
 
     case 13:
-        return (evaluate("-a", m, pf, answer) == 1  &&  answer == 999);
+        return check(evaluate("-a", m, pf, answer) == 1  &&  answer == 999);
 
     case 14:
-        return (evaluate("o/(-a+e)", m, pf, answer) == 1 && answer == 999);
+        return check(evaluate("o/(-a+e)", m, pf, answer) == 1 && answer == 999);
 
     case 15:
-        return (evaluate("   -a+b", m, pf, answer) == 1 && answer == 999);
+        return check(evaluate("   -a+b", m, pf, answer) == 1 && answer == 999);
 
     case 16:
-        return (evaluate("a*b", m, pf, answer) == 2  &&
+        return check(evaluate("a*b", m, pf, answer) == 2  &&
                pf == "ab*"  &&  answer == 999);
 
     case 17:
-        return (evaluate("y +o *(   a-u)  ", m, pf, answer) == 0 &&
+        return check(evaluate("y +o *(   a-u)  ", m, pf, answer) == 0 &&
                pf == "yoau-*+"  &&  answer == -1);
 
     case 18:
-        return (evaluate("o/(y-y)", m, pf, answer) == 3  &&
+        return check(evaluate("o/(y-y)", m, pf, answer) == 3  &&
                pf == "oyy-/"  &&  answer == 999);
 
     case 19:
-        return (evaluate(" a  ", m, pf, answer) == 0  &&
+        return check(evaluate(" a  ", m, pf, answer) == 0  &&
                pf == "a"  &&  answer == 3);
 
     case 20:
-        return (evaluate("((a))", m, pf, answer) == 0  &&
+        return check(evaluate("((a))", m, pf, answer) == 0  &&
                pf == "a"  &&  answer == 3);
 
     case 21:
-        return (evaluate("    (a+y)  ", m, pf, answer) == 0 &&
+        return check(evaluate("    (a+y)  ", m, pf, answer) == 0 &&
                pf == "ay+" && answer == 4);
 
     default:
@@ -115,7 +131,8 @@ int main() {
     for (int i = 0; i < TOTAL_TEST_CASE; ++i) {
         if (!test(i))
             cout << "*** FAILURE ***" << endl
-                 << "fails test case " << i << endl;
+                 << "fails test case " << i << endl
+                 << message << endl;
         else
             ++passed;
     }
